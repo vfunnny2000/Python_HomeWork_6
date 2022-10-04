7a10bceb563fed97901f1826fc32911755e72cb5
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-10-04 19:23:30 +0300

    Task 3

--- Seminar_3_HomeWork/Task3.py
+++ Seminar_3_HomeWork/Task3.py
@@ -6,6 +6,15 @@
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
 
+import os
+os.system('cls')
+
+lst = list(map(float, input("Введите числа через пробел:\n").split()))
+new_lst = [round(i%1,2) for i in lst if i%1 != 0]
+print(max(new_lst) - min(new_lst))
+
+
+
 # import os
 # os.system('cls')
 
@@ -28,10 +37,4 @@
 
 
 
-import os
-os.system('cls')
-
-lst = list(map(float, input("Введите числа через пробел:\n").split()))
-new_lst = [round(i%1,2) for i in lst if i%1 != 0]
-print(max(new_lst) - min(new_lst))
 
