7702dfa4d0af558624e23ac72acdafbb4855d2a4
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-10-08 19:52:42 +0300

    Task 1

--- Seminar_5_HomeWork/Task_1.py
+++ Seminar_5_HomeWork/Task_1.py
@@ -1,12 +1,30 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-newTxt = 'абвгедейка это детская передача. абвгдейка это учеба и игра'.split()
+# newTxt = 'абвгедейка это детская передача. абвгдейка это учеба и игра'
 
-strTxt = 'абв'
-count = 0
-for i in newTxt:
-    if strTxt in i:
-        newTxt.pop(count)
-    count += 1
-print(newTxt) 
+
+import os
+os.system('cls')
+
+# with open('file.txt', 'r') as data:
+#     text = data.read()
+#     print('Исходный текст: ', text)
+
+# find_txt = "абв"
+# lst = [i for i in text.split() if find_txt not in i]
+# print(f'Результат: {" ".join(lst)}')
+
+
+
+incoming_text = 'абвгедейка это детская передача. абвгдейка это учеба и игра'
+
+def del_words(incoming_text):
+    incoming_text = list(filter(lambda x: 'абв' not in x, incoming_text.split()))
+    return " ".join(incoming_text)
+
+incoming_text = del_words(incoming_text)
+print(incoming_text)
+
+
+
 
