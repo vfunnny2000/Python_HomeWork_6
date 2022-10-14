dc36f7dcfd95b65043b352acd59b443910afa8db
vfunnny2000 <OVDuganova@yandex.ru>
2022-10-14 22:12:55 +0300

    Task 4

--- Seminar_6_HomeWork/Task_4.py
+++ Seminar_6_HomeWork/Task_4.py
@@ -5,24 +5,24 @@ import os
 os.system('cls')
 
 
-# from itertools import accumulate
-# import operator
-
-# n = int(input('Введите число: '))
-
-# def get_prods(N):
-#     return list(accumulate([x for x in range(1, n + 1)], operator.mul))
-
-# print(get_prods(n))
-
-
-
-from math import factorial
+from itertools import accumulate
+import operator
 
 n = int(input('Введите число: '))
-f = lambda x: ((x == 1) and 1) or x * factorial(x -1)
-list2 = list( f(i) for i in range(1, n +1))
-print(list2)
+
+def get_prods(N):
+    return list(accumulate([x for x in range(1, n + 1)], operator.mul))
+
+print(get_prods(n))
+
+
+
+# from math import factorial
+
+# n = int(input('Введите число: '))
+# f = lambda x: ((x == 1) and 1) or x * factorial(x -1)
+# list2 = list( f(i) for i in range(1, n +1))
+# print(list2)
 
 
 
